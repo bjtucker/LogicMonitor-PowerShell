9daacb09f5bfb4e946a3f0a45b6730b2dc50d113
Ben Tucker <ben@youlose.org>
2016-11-23 01:57:36 -0600

    add Get-LMxAccount.ps1

---
+++ Get-LMxAccount.ps1
@@ -0,0 +1,36 @@
+function Get-LMxAccount {
+<#
+.SYNOPSIS
+    Retrieve account list from LogicMonitor portal. 
+.EXAMPLE
+    PS> Get-LMxAccount -Session $session
+    Return a list of all Account objects in the connected portal.
+#>
+    [CmdletBinding(PositionalBinding=$true)]
+    [Alias()]
+    [OutputType([String])]
+    Param ([Parameter(Mandatory=$true,
+                      Position=0,
+                      ValueFromPipeline=$true,
+                      ValueFromPipelineByPropertyName=$true,
+                      ValueFromRemainingArguments=$true)]
+            [ValidateNotNullOrEmpty()]
+            [Alias("Session")]
+            $LMSession
+           )
+    begin {
+ 
+        # if AccountId specified, use getAccount to retrieve single entity. 
+        $r = invoke-webrequest -UseBasicParsing -WebSession $LMSession.WebSession -Method Get -Uri "$($LMsession.BaseURI)rpc/getAccounts"
+        $Accounts = ($r.Content|ConvertFrom-Json).data
+    }
+
+    process {
+        $Accounts
+    }
+    
+}
+
+if ($MyInvocation.CommandOrigin -eq "Runspace") {
+    Get-LMxAccount @args
+}
